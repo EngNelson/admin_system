b6b1c008c4bbaee636da5caed3463c887c086a03
EngNelson <bechem200@gmail.com>
2022-03-19 23:47:30 +0100

    product area adjusted

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,7 +1,7 @@
 import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
-import DriveFolderUploadOutlinedIcon from "@mui/icons-material";
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
 
 const New = ({ inputs, title }) => {
